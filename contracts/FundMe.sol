75c1f092b9795ff02ed241e32e89b54f84b9f3a1
FerMusicComposer <lfernandosalamanca@gmail.com>
2025-05-15 01:35:46 +0200

    implemented withdraw function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -10,16 +10,44 @@ contract FundMe {
     using PriceConverter for uint256;
 
     uint256 public minimumAmount = 5e18;
+    address public immutable owner;
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only contract owner can execute this function");
+        _;
+    }
+    
     function fund() public payable {
         require(msg.value.getConversionRate() >= minimumAmount, "didn't send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {}
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex; funderIndex < funders.length; funderIndex++) 
+        {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
 
+        funders = new address[](0);
 
+        //Different ways of withdrawing from an address:
+        //1. transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        
+        //2. send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "send operation failed");
+        
+        //3. call (this is the recommended way of doing it)
+        (bool callSuccess,) = msg.sender.call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
 }
