ee2541cf63c9e1721f56d77f18272953c3ba0945
FerMusicComposer <lfernandosalamanca@gmail.com>
2025-05-15 01:00:34 +0200

    extracted price feed logic to library

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,31 +4,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 public minimumAmount = 5e18;
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
     function fund() public payable {
-        require(getConversionRate(msg.value ) >= minimumAmount, "didn't send enough ETH");
+        require(msg.value.getConversionRate() >= minimumAmount, "didn't send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
-    
+
     function withdraw() public {}
 
-    function getEthPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        
-        return uint256(price * 1e10);
-    }
 
-    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getEthPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
 }
